0c4758bf4c9b251d1592f0c2f3cd2a447586c921
Artur Ceschin <artur.ceschin@gmail.com>
2023-10-27 17:45:50 -0300

    feat: initial style and customization

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,38 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ['var(--font-poppins)'],
+      },
+      colors: {
+        background: '#FAFAFB',
+        primary: '#2F80ED',
+        gray: {
+          '100': '#F6F8FB',
+          '200': '#E0E0E0',
+          '300': '#BDBDBD',
+          '400': '#828282',
+          '500': '#4F4F4F',
+        },
+        green: '#219653',
+        'light-blue': '#97BEF4',
+      },
+
+      keyframes: {
+        moveHorizontal: {
+          '0%': {
+            transform: 'translateX(0)',
+          },
+          '50%': {
+            transform: 'translateX(75%)',
+          },
+          '100%': {
+            transform: 'translateX(0)',
+          },
+        },
+      },
+      animation: {
+        moveHorizontal: '4s linear infinite moveHorizontal',
       },
     },
   },
