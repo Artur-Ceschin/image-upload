bbca12c90179a232c84c6aa07e92f74676a11cf8
Artur Ceschin <artur.ceschin@gmail.com>
2023-11-02 10:23:56 -0300

    refactor: changed toast to an isolated componente

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,9 @@ import { useState } from 'react'
 import { UploadImage } from '@/components/UploadImage'
 import { UploadLoading } from '@/components/UploadLoading'
 import { ImagePreview } from '@/components/ImagePreview'
-import { ToastContainer } from 'react-toastify'
 
 import 'react-toastify/dist/ReactToastify.css'
+import { Toast } from '@/components/Toast'
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(false)
@@ -14,19 +14,7 @@ export default function Home() {
 
   return (
     <>
-      <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme="light"
-      />
-
+      <Toast />
       <main className="bg-background w-full h-screen flex items-center justify-center">
         <div className="w-full max-w-[420px] bg-white rounded-xl drop-shadow-md p-8">
           {isLoading ? (
---
+++ src/components/Toast.tsx
@@ -0,0 +1,18 @@
+import { ToastContainer } from 'react-toastify'
+
+export function Toast() {
+  return (
+    <ToastContainer
+      position="top-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop={false}
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss={false}
+      draggable
+      pauseOnHover
+      theme="light"
+    />
+  )
+}
